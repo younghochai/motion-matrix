2f83c6c7ebbcbe2c6b4d5f338fead805c6b95d16
Adithya B <adithyakoundinya@gmail.com>
2019-09-10 16:31:47 +0900

    AddingPecentChange
    Adding Pecentage of Match.
    Modified files:
    - Comparision.h
    - Comparision.cpp
    - Robot_Xsens.cpp
    
    Updating CMakeList
    - Adding PostBuild Rules to create an empty CData Directory

--- APISource/include/Comparision.h
+++ APISource/include/Comparision.h
@@ -10,6 +10,6 @@ using namespace std;
 class Comparision {
 	
 	public:static float getAngularDistance(TVector3 q1, TVector3 q2);
-	public:static float getDiffBtwTrajectory(char* usf1File, char* lsf1File, char* usf2File, char* lsf2File);
+	public:static float getDiffBtwTrajectory(char* usf1File, char* lsf1File, char* usf2File, char* lsf2File, int &percent);
 	public:static void readCSV(istream &input, vector< vector<string> > &output, quaternion(&trajectory)[1024], int &count);
 };
