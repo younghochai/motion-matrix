dc2d525fdfa3d31ca251e2cad30794e69c752e0b
Adithya B <adithyakoundinya@gmail.com>
2021-02-08 15:56:28 +0900

    Adding Motion Editing feature to the Motion sphere

--- OpenIA/Source/iaVitruvianAvatar.cpp
+++ OpenIA/Source/iaVitruvianAvatar.cpp
@@ -1,6 +1,7 @@
 #include "iaVitruvianAvatar.h"
 #include <vtkParametricSuperEllipsoid.h>
 #include <vtkParametricFunctionSource.h>
+#include <vtkGenericOpenGLRenderWindow.h>
 
 Avatar VitruvianAvatar::vitruvianAvatarUpdate;
 bool VitruvianAvatar::isLoaded = false;
@@ -19,6 +20,10 @@ typedef struct vetruvianVtkAvatarSegment
 	vtkSmartPointer<vtkSphereSource> sphereSource = vtkSmartPointer<vtkSphereSource>::New();
 	vtkSmartPointer<vtkPolyDataMapper> sphereMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
 	vtkSmartPointer<vtkActor> sphereActor = vtkSmartPointer<vtkActor>::New();
+
+	vtkSmartPointer<vtkSphereSource> jointSource = vtkSmartPointer<vtkSphereSource>::New();
+	vtkSmartPointer<vtkPolyDataMapper> jointMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+	vtkSmartPointer<vtkActor> jointActor = vtkSmartPointer<vtkActor>::New();
 	double jointPoint[3];
 	float length;
 } JointSegment;
@@ -32,6 +37,8 @@ typedef struct vetruvianTorso
 	vtkSmartPointer<vtkSphereSource> sphereSource = vtkSmartPointer<vtkSphereSource>::New();
 	vtkSmartPointer<vtkPolyDataMapper> sphereMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
 	vtkSmartPointer<vtkActor> sphereActor = vtkSmartPointer<vtkActor>::New();
+
+	
 	double jointPoint[3];
 	float length;
 } Torso;
@@ -80,10 +87,24 @@ void drawSphere(JointSegment &jointSegment, float radius)
 	renderer->AddActor(jointSegment.sphereActor);
 }
 
+void drawJointSphere(JointSegment &jointSegment, float radius)
+{
+	jointSegment.jointSource->SetCenter(jointSegment.jointPoint);
+	jointSegment.jointSource->SetRadius(radius);
+	jointSegment.jointSource->SetPhiResolution(50);
+	jointSegment.jointSource->SetThetaResolution(50);
+	jointSegment.jointMapper->SetInputConnection(jointSegment.jointSource->GetOutputPort());
+
+	jointSegment.jointActor->SetMapper(jointSegment.jointMapper);
+	jointSegment.jointActor->GetProperty()->SetColor(colors->GetColor3d("White").GetData());
+	jointSegment.jointActor->GetProperty()->SetOpacity(0.5);
+	//jointSegment.jointActor->GetProperty()->SetSpecularPower(20);
+
+	renderer->AddActor(jointSegment.jointActor);
+}
+
 void drawCube(JointSegment& handJoint, Hand& hand, float size)
 {
-	
-
 	/*hand.handSource->SetCenter(handJoint.jointPoint);
 	hand.handSource->SetXLength(size - 2);
 	hand.handSource->SetYLength(size + 1);
@@ -301,10 +322,11 @@ void drawBoneSegment(JointSegment &startSegment, JointSegment &endSegment, float
 	startSegment.cylinderActor->GetProperty()->SetSpecular(.5);
 	startSegment.cylinderActor->GetProperty()->SetSpecularPower(20);
 	//draw start and end points as a sphere
-	if (!isHead)
+	drawSphere(startSegment, 3);
+	drawSphere(endSegment, 3);
+	//if (!isHead)
 	{
-		drawSphere(startSegment, 3);
-		drawSphere(endSegment, 3);
+		//drawJointSphere(startSegment, 8);
 	}
 	
 
@@ -392,6 +414,7 @@ void drawFloor()
 	vtkSmartPointer<vtkPlaneSource> planeSource =
 		vtkSmartPointer<vtkPlaneSource>::New();
 	planeSource->SetCenter(0.0, -0.0015, 0.0);
+	//planeSource->SetCenter(0.0, 0.005, 0.0);
 	planeSource->SetNormal(0.0, 1.0, 0.0);
 	//planeSource->set
 	planeSource->Update();
@@ -425,12 +448,12 @@ void drawAvatar()
 	float scaling[3];
 
 	scaling[0] = 2.5; scaling[1] = 0.5; scaling[2] = 2.5;
-	drawBoneSegment(rightFootLowerLeg, rightKneeUpperLeg,scaling, false,rllc); // right lower leg
+	drawBoneSegment(rightFootLowerLeg, rightKneeUpperLeg,scaling, true,rllc); // right lower leg
 	scaling[0] = 3.5; scaling[1] = 0.5; scaling[2] = 3.5;
 	drawBoneSegment(rightKneeUpperLeg, rightHipSegment, scaling, false, rulc); // right upper leg
 
 	scaling[0] = 2.5; scaling[1] = 0.5; scaling[2] = 2.5;
-	drawBoneSegment(leftFootLowerLeg, leftKneeUpperLeg,scaling, false, lllc); // left lower leg
+	drawBoneSegment(leftFootLowerLeg, leftKneeUpperLeg,scaling, true, lllc); // left lower leg
 	scaling[0] = 3.5; scaling[1] = 0.5; scaling[2] = 3.5;
 	drawBoneSegment(leftKneeUpperLeg, leftHipSegment,scaling, false, lulc); // left upper leg
 
@@ -453,14 +476,14 @@ void drawAvatar()
 	drawBoneSegment(rightShoulderSegment, neckToChin,scaling, false, "DimGray"); // right shoulder
 	drawBoneSegment(rightElbowUpperArm, rightShoulderSegment,scaling, false, ruhc); // right upper arm
 	scaling[0] = 2.5; scaling[1] = 0.5; scaling[2] = 2.5;
-	drawBoneSegment(rightHandLowerArm, rightElbowUpperArm,scaling, false, rlhc); // right lower arm
+	drawBoneSegment(rightHandLowerArm, rightElbowUpperArm,scaling, true, rlhc); // right lower arm
 	drawCube(rightHandLowerArm, rightHand, 15);
 
 	scaling[0] = 3.5; scaling[1] = 0.5; scaling[2] = 3.5;
 	drawBoneSegment(leftShoulderSegment, neckToChin,scaling, false, "DimGray"); // left shoulder
 	drawBoneSegment(leftElbowUpperArm, leftShoulderSegment,scaling, false, luhc); // left upper arm
 	scaling[0] = 2.5; scaling[1] = 0.5; scaling[2] = 2.5;
-	drawBoneSegment(leftHandLowerArm, leftElbowUpperArm,scaling, false, llhc); // left lower arm
+	drawBoneSegment(leftHandLowerArm, leftElbowUpperArm,scaling, true, llhc); // left lower arm
 	drawCube(leftHandLowerArm, leftHand, 15);
 }
 
@@ -548,6 +571,7 @@ void rotateAvatar(Avatar avatar)
 		pelvisStomach.jointPoint[0] = rightHipSegment.jointPoint[0] + vec.mData[0] * rightHipSegment.length;
 		pelvisStomach.jointPoint[1] = rightHipSegment.jointPoint[1] + vec.mData[2] * rightHipSegment.length;
 		pelvisStomach.jointPoint[2] = rightHipSegment.jointPoint[2] - vec.mData[1] * rightHipSegment.length;
+		//cout << pelvisStomach.jointPoint[0] << "," << pelvisStomach.jointPoint[1] << "," << pelvisStomach.jointPoint[2] << ",";
 		
 		/*float diffR = rightHipSegment.jointPoint[1] - leftHipSegment.jointPoint[1];
 		leftKneeUpperLeg.jointPoint[1]	= leftKneeUpperLeg.jointPoint[1] + diffR;
@@ -567,7 +591,7 @@ void rotateAvatar(Avatar avatar)
 		pelvisStomach.jointPoint[2] = (rightHipSegment.jointPoint[2] + leftHipSegment.jointPoint[2]) / 2;
 		isEqual = true;
 	}*/
-
+	cout << pelvisStomach.jointPoint[0] << "," << pelvisStomach.jointPoint[1] << "," << pelvisStomach.jointPoint[2]<<endl;
 //----------------------------------------Top-Down Update---------------------------------------------------------------------------------------
 		//Finding right pelvis joint from the pelvis.
 		q = avatar.b0;// .Inverse().mutiplication(avatar.b0);
@@ -576,6 +600,7 @@ void rotateAvatar(Avatar avatar)
 		rightHipSegment.jointPoint[0] = pelvisStomach.jointPoint[0] + vec.mData[0] * rightHipSegment.length;
 		rightHipSegment.jointPoint[1] = pelvisStomach.jointPoint[1] + vec.mData[2] * rightHipSegment.length; //Changes in Lower body with pelvis rotation and model moves continously in z axis
 		rightHipSegment.jointPoint[2] = pelvisStomach.jointPoint[2] - vec.mData[1] * rightHipSegment.length;//Changes in Lower body with pelvis rotation and model moves continously in z axis
+		//cout << rightHipSegment.jointPoint[0] << "," << rightHipSegment.jointPoint[1] << "," << rightHipSegment.jointPoint[2] << ",";
 
 		//Finding left pelvis joint from the pelvis.
 		q = avatar.b0;// .Inverse().mutiplication(avatar.b0);
@@ -584,6 +609,7 @@ void rotateAvatar(Avatar avatar)
 		leftHipSegment.jointPoint[0] = pelvisStomach.jointPoint[0] + vec.mData[0] * leftHipSegment.length;
 		leftHipSegment.jointPoint[1] = pelvisStomach.jointPoint[1] + vec.mData[2] * leftHipSegment.length;  //Changes in Lower body with pelvis rotation and model moves continously in z axis
 		leftHipSegment.jointPoint[2] = pelvisStomach.jointPoint[2] - vec.mData[1] * leftHipSegment.length;	//Changes in Lower body with pelvis rotation and model moves continously in z axis
+		//cout << leftHipSegment.jointPoint[0] << "," << leftHipSegment.jointPoint[1] << "," << leftHipSegment.jointPoint[2] << ",";
 
 	//if (!isEqual) //Execute topdown approach only when left and right hips are unequal
 	{
@@ -594,6 +620,7 @@ void rotateAvatar(Avatar avatar)
 		rightKneeUpperLeg.jointPoint[0] = (rightHipSegment.jointPoint[0] + vec.mData[0] * rightKneeUpperLeg.length);
 		rightKneeUpperLeg.jointPoint[1] = rightHipSegment.jointPoint[1] + vec.mData[2] * rightKneeUpperLeg.length;
 		rightKneeUpperLeg.jointPoint[2] = rightHipSegment.jointPoint[2] + -vec.mData[1] * rightKneeUpperLeg.length;
+		//cout << rightKneeUpperLeg.jointPoint[0] << "," << rightKneeUpperLeg.jointPoint[1] << "," << rightKneeUpperLeg.jointPoint[2] << ",";
 
 		//Finding left knee from the left pelvis point
 		q = avatar.b8;// .Inverse().mutiplication(avatar.b8);
@@ -602,6 +629,7 @@ void rotateAvatar(Avatar avatar)
 		leftKneeUpperLeg.jointPoint[0] = leftHipSegment.jointPoint[0] + vec.mData[0] * leftKneeUpperLeg.length;
 		leftKneeUpperLeg.jointPoint[1] = leftHipSegment.jointPoint[1] + vec.mData[2] * leftKneeUpperLeg.length;
 		leftKneeUpperLeg.jointPoint[2] = leftHipSegment.jointPoint[2] + -vec.mData[1] * leftKneeUpperLeg.length;
+		//cout << leftKneeUpperLeg.jointPoint[0] << "," << leftKneeUpperLeg.jointPoint[1] << "," << leftKneeUpperLeg.jointPoint[2] << ",";
 
 		//Finding right foot from the right knee point
 		q = avatar.b7;// .Inverse().mutiplication(avatar.b7);
@@ -610,6 +638,7 @@ void rotateAvatar(Avatar avatar)
 		rightFootLowerLeg.jointPoint[0] = rightKneeUpperLeg.jointPoint[0] + vec.mData[0] * rightFootLowerLeg.length;
 		rightFootLowerLeg.jointPoint[1] = rightKneeUpperLeg.jointPoint[1] + vec.mData[2] * rightFootLowerLeg.length;
 		rightFootLowerLeg.jointPoint[2] = rightKneeUpperLeg.jointPoint[2] + -vec.mData[1] * rightFootLowerLeg.length;
+		//cout << rightFootLowerLeg.jointPoint[0] << "," << rightFootLowerLeg.jointPoint[1] << "," << rightFootLowerLeg.jointPoint[2] << ",";
 
 		//Finding left foot from the left knee point
 		q = avatar.b9;// .Inverse().mutiplication(avatar.b9);
@@ -618,6 +647,7 @@ void rotateAvatar(Avatar avatar)
 		leftFootLowerLeg.jointPoint[0] = leftKneeUpperLeg.jointPoint[0] + vec.mData[0] * leftFootLowerLeg.length;
 		leftFootLowerLeg.jointPoint[1] = leftKneeUpperLeg.jointPoint[1] + vec.mData[2] * leftFootLowerLeg.length;;
 		leftFootLowerLeg.jointPoint[2] = leftKneeUpperLeg.jointPoint[2] + -vec.mData[1] * leftFootLowerLeg.length;
+		//cout << leftFootLowerLeg.jointPoint[0] << "," << leftFootLowerLeg.jointPoint[1] << "," << leftFootLowerLeg.jointPoint[2] << ",";
 	}
 	//---------------------------------------------Updating UpperBody starting from pelvis position --------------------
 	//Finding sternum from the pelvis point
@@ -627,7 +657,7 @@ void rotateAvatar(Avatar avatar)
 	sternumChest.jointPoint[0] = pelvisStomach.jointPoint[0] + vec.mData[0] * pelvisStomach.length;
 	sternumChest.jointPoint[1] = pelvisStomach.jointPoint[1] + vec.mData[2] * pelvisStomach.length;
 	sternumChest.jointPoint[2] = pelvisStomach.jointPoint[2] + -vec.mData[1] * pelvisStomach.length;
-
+	//cout << sternumChest.jointPoint[0] << "," << sternumChest.jointPoint[1] << "," << sternumChest.jointPoint[2] << ",";
 	//Finding neck from the sternum point
 	q = avatar.b1;// .Inverse().mutiplication(avatar.b1);
 	vec = q.mutiplication(quaternion{ 0,0,1,0 }.mutiplication(q.Inverse()));
@@ -635,7 +665,7 @@ void rotateAvatar(Avatar avatar)
 	neckToChin.jointPoint[0] = sternumChest.jointPoint[0] + vec.mData[0] * sternumChest.length;
 	neckToChin.jointPoint[1] = sternumChest.jointPoint[1] + vec.mData[2] * sternumChest.length;
 	neckToChin.jointPoint[2] = sternumChest.jointPoint[2] + -vec.mData[1] * sternumChest.length;
-
+	//cout << neckToChin.jointPoint[0] << "," << neckToChin.jointPoint[1] << "," << neckToChin.jointPoint[2] << ",";
 	headQuat = avatar.b1; //assigning headQuat for head rotation
 
 	//Finding head from the neck point
@@ -649,7 +679,7 @@ void rotateAvatar(Avatar avatar)
 	head.jointPoint[0] = chinToHead.jointPoint[0] + vec.mData[0] * chinToHead.length;
 	head.jointPoint[1] = chinToHead.jointPoint[1] + vec.mData[2] * chinToHead.length;
 	head.jointPoint[2] = chinToHead.jointPoint[2] + -vec.mData[1] * chinToHead.length;
-
+	//cout << head.jointPoint[0] << "," << head.jointPoint[1] << "," << head.jointPoint[2] << ",";
 	//---------------------------------------------- Hands ----------------------------------------------------
 		//Finding rightShoulder from the neck point
 	q = avatar.b1;// .Inverse().mutiplication(avatar.b1);
@@ -658,7 +688,7 @@ void rotateAvatar(Avatar avatar)
 	rightShoulderSegment.jointPoint[0] = neckToChin.jointPoint[0] + vec.mData[0] * rightShoulderSegment.length;
 	rightShoulderSegment.jointPoint[1] = neckToChin.jointPoint[1] + vec.mData[2] * rightShoulderSegment.length;
 	rightShoulderSegment.jointPoint[2] = neckToChin.jointPoint[2] + -vec.mData[1] * rightShoulderSegment.length;
-
+	//cout << rightShoulderSegment.jointPoint[0] << "," << rightShoulderSegment.jointPoint[1] << "," << rightShoulderSegment.jointPoint[2] << ",";
 	//Finding leftShoulder from the neck point
 	q = avatar.b1;// .Inverse().mutiplication(avatar.b1);
 	vec = q.mutiplication(quaternion{ 1,0,0,0 }.mutiplication(q.Inverse()));
@@ -666,41 +696,45 @@ void rotateAvatar(Avatar avatar)
 	leftShoulderSegment.jointPoint[0] = neckToChin.jointPoint[0] + vec.mData[0] * leftShoulderSegment.length;
 	leftShoulderSegment.jointPoint[1] = neckToChin.jointPoint[1] + vec.mData[2] * leftShoulderSegment.length;
 	leftShoulderSegment.jointPoint[2] = neckToChin.jointPoint[2] + -vec.mData[1] * leftShoulderSegment.length;
-
+	//cout << leftShoulderSegment.jointPoint[0] << "," << leftShoulderSegment.jointPoint[1] << "," << leftShoulderSegment.jointPoint[2] << ",";
 	//Finding right elbow from the right shoulder point
 	q = avatar.b2;// .Inverse().mutiplication(avatar.b2);
-	vec = q.mutiplication(quaternion{ 0,0,-1,0 }.mutiplication(q.Inverse()));
+	vec = q.mutiplication(quaternion{ 0,0,-1,0 }.mutiplication(q.Inverse())); // Attention Pose
+	//vec = q.mutiplication(quaternion{ -1,0,0,0 }.mutiplication(q.Inverse())); // T-Pose
 
 	rightElbowUpperArm.jointPoint[0] = rightShoulderSegment.jointPoint[0] + vec.mData[0] * rightElbowUpperArm.length;
 	rightElbowUpperArm.jointPoint[1] = rightShoulderSegment.jointPoint[1] + vec.mData[2] * rightElbowUpperArm.length;
 	rightElbowUpperArm.jointPoint[2] = rightShoulderSegment.jointPoint[2] + -vec.mData[1] * rightElbowUpperArm.length;
-
+	//cout << rightElbowUpperArm.jointPoint[0] << "," << rightElbowUpperArm.jointPoint[1] << "," << rightElbowUpperArm.jointPoint[2] << ",";
 	//Finding left elbow from the left shoulder point
 	q = avatar.b4;// .Inverse().mutiplication(avatar.b4);
-	vec = q.mutiplication(quaternion{ 0,0,-1,0 }.mutiplication(q.Inverse()));
+	vec = q.mutiplication(quaternion{ 0,0,-1,0 }.mutiplication(q.Inverse())); // Attention Pose
+	//vec = q.mutiplication(quaternion{ 1,0,0,0 }.mutiplication(q.Inverse())); // T-Pose
 
 	leftElbowUpperArm.jointPoint[0] = leftShoulderSegment.jointPoint[0] + vec.mData[0] * leftElbowUpperArm.length;
 	leftElbowUpperArm.jointPoint[1] = leftShoulderSegment.jointPoint[1] + vec.mData[2] * leftElbowUpperArm.length;
 	leftElbowUpperArm.jointPoint[2] = leftShoulderSegment.jointPoint[2] + -vec.mData[1] * leftElbowUpperArm.length;
-
+	//cout << leftElbowUpperArm.jointPoint[0] << "," << leftElbowUpperArm.jointPoint[1] << "," << leftElbowUpperArm.jointPoint[2] << ",";
 	//Finding right hand from the right elbow point
 	q = avatar.b3;// .Inverse().mutiplication(avatar.b3);
 	rightHand.rotation = q;
-	vec = q.mutiplication(quaternion{ 0,0,-1,0 }.mutiplication(q.Inverse()));
+	vec = q.mutiplication(quaternion{ 0,0,-1,0 }.mutiplication(q.Inverse())); // Attention pose
+	//vec = q.mutiplication(quaternion{ -1,0,0,0 }.mutiplication(q.Inverse())); // t-Pose
 
 	rightHandLowerArm.jointPoint[0] = rightElbowUpperArm.jointPoint[0] + vec.mData[0] * rightHandLowerArm.length;
 	rightHandLowerArm.jointPoint[1] = rightElbowUpperArm.jointPoint[1] + vec.mData[2] * rightHandLowerArm.length;
 	rightHandLowerArm.jointPoint[2] = rightElbowUpperArm.jointPoint[2] + -vec.mData[1] * rightHandLowerArm.length;
-
+	//cout << rightHandLowerArm.jointPoint[0] << "," << rightHandLowerArm.jointPoint[1] << "," << rightHandLowerArm.jointPoint[2]<<",";
 	//Finding left hand from the left elbow point
 	q = avatar.b5;// .Inverse().mutiplication(avatar.b5);
 	leftHand.rotation = q;
-	vec = q.mutiplication(quaternion{ 0,0,-1,0 }.mutiplication(q.Inverse()));
+	vec = q.mutiplication(quaternion{ 0,0,-1,0 }.mutiplication(q.Inverse())); // Attention Pose
+	//vec = q.mutiplication(quaternion{ 1,0,0,0 }.mutiplication(q.Inverse())); // T-pose
 
 	leftHandLowerArm.jointPoint[0] = leftElbowUpperArm.jointPoint[0] + vec.mData[0] * leftHandLowerArm.length;
 	leftHandLowerArm.jointPoint[1] = leftElbowUpperArm.jointPoint[1] + vec.mData[2] * leftHandLowerArm.length;
 	leftHandLowerArm.jointPoint[2] = leftElbowUpperArm.jointPoint[2] + -vec.mData[1] * leftHandLowerArm.length;
-
+	//cout << leftHandLowerArm.jointPoint[0] << "," << leftHandLowerArm.jointPoint[1] << "," << leftHandLowerArm.jointPoint[2]<<endl;
 	drawAvatar();
 }
 
@@ -1066,9 +1100,9 @@ public:
 			i = 1;
 			VitruvianAvatar::isLoaded = true;
 		}
-		if (key == "m")
+		if (key == "b")
 		{
-			
+			VitruvianAvatar::initializeVetruvianVtkAvatar();
 		}
 		// Forward events
 		vtkInteractorStyleTrackballCamera::OnKeyPress();
@@ -1231,6 +1265,11 @@ void VitruvianAvatar::startVetruvianAvatar()
 	renderWindowInteractor->Start();
 }
 
+vtkSmartPointer<vtkRenderer> VitruvianAvatar::getVitruvianAvatarRenderer()
+{
+	return renderer;
+}
+
 void KeypressCallbackFunction(vtkObject* caller, long unsigned int vtkNotUsed(eventId), void* vtkNotUsed(clientData), void* vtkNotUsed(callData))
 {
 	vtkRenderWindowInteractor *iren =
@@ -1263,5 +1302,9 @@ void KeypressCallbackFunction(vtkObject* caller, long unsigned int vtkNotUsed(ev
 		style->SetDefaultRenderer(renderer);
 
 		renderWindowInteractor->SetInteractorStyle(style);
+	}
+	if (key == "b")
+	{
+		VitruvianAvatar::initializeVetruvianVtkAvatar();
 	}
 }
