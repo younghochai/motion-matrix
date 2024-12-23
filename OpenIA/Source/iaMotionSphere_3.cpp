08c145badc0c0fa3cea7daaa70e40c1412b94a57
KIMJOUNGJUN-maybenot <ingiproject3@gmail.com>
2024-12-23 12:22:56 +0900

    Update iaMotionSphere_3.cpp

--- OpenIA/Source/iaMotionSphere_3.cpp
+++ OpenIA/Source/iaMotionSphere_3.cpp
@@ -13,6 +13,7 @@ MotionSphere ms;
 SphereUtility expertSU;
 Model_PLY rightHandPLY, leftHandPLY, rightFootPLY, leftFootPLY, kneePLY, elbowPLY;
 
+//remeberme
 iaAcquireGesture AcquireSFQ2;
 // edit complite
 struct PixelColor {
